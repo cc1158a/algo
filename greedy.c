ed116ecf5fe30b76525705129ab6623a67f9c164
陈泽 <8105518+chenzehao19910509@user.noreply.gitee.com>
2025-03-26 15:06:31 +0800

    m

---
+++ greedy.c
@@ -0,0 +1,61 @@
+ #include "stdlib.h"
+ #include "greedy.h"
+/**
+ * 设有 m 台完全相同的机器运行 n 个独立的任务，运行任务 i 所需要的时间为 ti。
+ * 要求确 定一个调度方案，使得完成所有任务所需要的时间最短。
+ *  假设任务已经按照其运行时间从大到小排序，
+ * 算法基于最长运行时间作业优先的策略： 按顺序先把每个任务分配到一台机器上，
+ * 然后将剩余任务一一次放入最先空闲的机器。
+ */
+
+int scheduling_problem(int *task, int n, int m)
+{
+    if (n < m)
+    {
+        if (task[0])
+        {
+            return task[0];
+        }
+    }
+    int *machine = (int *)malloc(sizeof(int) * m);
+    int j = 0;
+    for (j = 0; j < n; j++)
+    {
+        machine[j] = task[j];
+    }
+    int min = task[0];
+    int t = 0;
+    while (j < n)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            if (machine[i] < min)
+            {
+                min = machine[i];
+            }
+        }
+        for (int i = 0; i < m; i++)
+        {
+            machine[i] -= min;
+            if (machine[i] == 0)
+            {
+               if (j < n)
+                {
+                    j++;
+                    machine[i] = task[j];
+                }
+            }
+        }
+
+        t+=min;        
+    }
+    int max=0;
+    for(int i=0;i<m;i++){
+        if(max<machine[i]){
+            max=machine[i];
+        }
+    }
+    t+=max;
+    free(machine);
+    return t;
+}
--- main.c
+++ main.c
@@ -1,12 +1,18 @@
 #include "stdio.h"
 #include "sort.h"
+#include "greedy.h"
 int main(){
-    int array1[] = {1, 2, 3, 4, 5};
-    int array2[] = {5, 4, 3, 2, 1};
-    int array3[] = {5, 1, 4, 1, 3};
+    // int array1[] = {1, 2, 3, 4, 5};
+    // int array2[] = {5, 4, 3, 2, 1};
+    // int array3[] = {5, 1, 4, 1, 3};
 
-    qs(array1, 5);
-    qs(array2, 5);
-    qs(array3, 5);
+    // qs(array1, 5);
+    // qs(array2, 5);
+    // qs(array3, 5);
+
+    int tasks[] = {8, 4, 3, 2, 1}; // 任务时间
+    int n = sizeof(tasks) / sizeof(tasks[0]);
+    int m = 2; // 机器数量
+    scheduling_problem(tasks,n,m);
     printf("done");
 }
